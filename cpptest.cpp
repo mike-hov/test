62a04e7adb6fba96ce73539ba427db94ee932896
mike-hov <mhovdesven@gwu.edu>
2017-06-22 09:58:42 +0200

    Create cpptest.cpp

---
+++ cpptest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout << "Enter value a";
+	cin >> a;
+	cout << "Enter value b";
+	cin >> b;
+	cout << "The sum of a and b is" << a+b;
+	return 0;
+}
